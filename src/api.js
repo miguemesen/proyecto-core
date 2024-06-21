314e22daf5f87854d7a54185d99c0a06a9ab5752
miguemesen <miguemesen12@gmail.com>
2024-06-21 02:32:56 -0600

    fix cors

--- src/api.js
+++ src/api.js
@@ -4,6 +4,11 @@ const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
 
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 router.get('/', (req, res) => {
   res.json({
     'hello': 'hi'
@@ -15,6 +20,13 @@ router.get('/test', (req, res) => {
     'test': 'hello test'
   })
 })
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use('/.netlify/functions/api', router)
 
